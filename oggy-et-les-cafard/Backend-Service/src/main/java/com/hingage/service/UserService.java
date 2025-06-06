0dfe36a5f2a1d74696b62d287eae8a0d304f0450
muchosalexis <alexis.rochefort@gmail.com>
2025-06-06 20:57:19 +0200

    adding ready-to-deploy features and code cleaned

--- oggy-et-les-cafard/Backend-Service/src/main/java/com/hingage/service/UserService.java
+++ oggy-et-les-cafard/Backend-Service/src/main/java/com/hingage/service/UserService.java
@@ -3,6 +3,8 @@ package com.hingage.service;
 import com.hingage.model.Users;
 import com.hingage.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
